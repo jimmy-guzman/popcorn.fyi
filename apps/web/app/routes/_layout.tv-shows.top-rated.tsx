c7e68de2bf2894a574d52a1dd1536e0b852db5a2
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2024-12-12 21:44:34 -0600

    feat: ✨ add top rated TV shows (#39)

--- apps/web/app/routes/_layout.tv-shows.top-rated.tsx
+++ apps/web/app/routes/_layout.tv-shows.top-rated.tsx
@@ -1,18 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { useServerFn } from "@tanstack/start";
+
+import { TVShowList } from "@/components/tv-show-list";
+import { topRatedTVShows } from "@/config/lists";
+import { topRatedTvFn } from "@/server/fn";
 
 export const Route = createFileRoute("/_layout/tv-shows/top-rated")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const getPopularTv = useServerFn(topRatedTvFn);
+
+  const { data: tvShows } = useQuery({
+    queryFn: () => {
+      return getPopularTv();
+    },
+    queryKey: ["tv/top-rated"],
+  });
+
   return (
-    <div className="grid min-h-[calc(100vh-8rem)] place-content-center">
-      <div className="prose dsy-prose">
-        <h1>
-          <span className="icon-[lucide--construction] h-7 w-7" /> Top Rated TV
-          Shows
-        </h1>
-      </div>
+    <div className="p-8">
+      <TVShowList
+        description={topRatedTVShows.description}
+        title={topRatedTVShows.title}
+        tvShows={tvShows?.results ?? []}
+      />
     </div>
   );
 }
