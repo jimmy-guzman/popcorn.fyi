b5bacbe978b6e8ca86630ad75eed7cbad9a60afe
jimmy-guzman <hi@jimmyguzman.com>
2024-12-16 17:38:20 -0600

    feat: ✨ toggle collapsible menu items on blur

--- apps/web/app/components/site-nav-menu-item.tsx
+++ apps/web/app/components/site-nav-menu-item.tsx
@@ -1,33 +1,50 @@
 import { useLocation } from "@tanstack/react-router";
+import { useRef } from "react";
 
-import type { NavItem } from "@/config/nav";
+import type { MultipleNavItem, NavItem } from "@/config/nav";
 
 import { cn } from "@/lib/cn";
 
 import { SiteNavMenuItemLink } from "./site-nav-menu-item-link";
 
-export const SiteNavMenuItem = ({ item }: { item: NavItem }) => {
+const SiteNavCollapsibleMenuItem = ({ item }: { item: MultipleNavItem }) => {
   const { pathname } = useLocation();
+  const ref = useRef<HTMLDetailsElement>(null);
 
+  const handleBlur = (e: React.FocusEvent<HTMLDetailsElement>) => {
+    const currentTarget = e.currentTarget;
+
+    requestAnimationFrame(() => {
+      if (!currentTarget.contains(document.activeElement) && ref.current) {
+        ref.current.open = false;
+      }
+    });
+  };
+  const isActive = pathname.startsWith(item.to);
+
+  return (
+    <details onBlur={handleBlur} ref={ref}>
+      <summary className={cn(isActive && "dsy-btn-active")}>
+        <span className={cn(item.icon, "h-5 w-5")} /> {item.title}
+      </summary>
+      <ul className="bg-base-100 rounded-t-none">
+        {item.items.map((item) => {
+          return (
+            <li key={item.title}>
+              <SiteNavMenuItemLink item={item} />
+            </li>
+          );
+        })}
+      </ul>
+    </details>
+  );
+};
+
+export const SiteNavMenuItem = ({ item }: { item: NavItem }) => {
   if ("items" in item) {
-    const isActive = pathname.startsWith(item.to);
-
     return (
       <li>
-        <details>
-          <summary className={cn(isActive && "dsy-btn-active")}>
-            <span className={cn(item.icon, "h-5 w-5")} /> {item.title}
-          </summary>
-          <ul className="bg-base-100 rounded-t-none">
-            {item.items.map((item) => {
-              return (
-                <li key={item.title}>
-                  <SiteNavMenuItemLink item={item} />
-                </li>
-              );
-            })}
-          </ul>
-        </details>
+        <SiteNavCollapsibleMenuItem item={item} />
       </li>
     );
   }
