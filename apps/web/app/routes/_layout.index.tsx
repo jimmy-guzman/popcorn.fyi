b1079d51fe4fa5e197cc395511b16c71d119a575
jimmy-guzman <hi@jimmyguzman.com>
2024-12-21 10:44:50 -0600

    fix: 🐛 no more hydration warnings on home page
    🏁 Closes: #69

--- apps/web/app/routes/_layout.index.tsx
+++ apps/web/app/routes/_layout.index.tsx
@@ -6,30 +6,33 @@ import { MovieHero } from "@/components/movie-hero";
 import { TvShowHero } from "@/components/tv-show-hero";
 import { trendingMoviesOptions, trendingTVOptions } from "@/lib/trending";
 
+export const Route = createFileRoute("/_layout/")({
+  component: Home,
+  loader: async ({ context }) => {
+    await Promise.all([
+      context.queryClient.ensureQueryData(trendingMoviesOptions()),
+      context.queryClient.ensureQueryData(trendingTVOptions()),
+    ]);
+
+    return [random(0, 1), random(0, 24)];
+  },
+});
+
 function Home() {
+  const [shouldShowMovie, randomTrending] = Route.useLoaderData();
   const { movie, tvShow } = useSuspenseQueries({
     combine: ([{ data: tvShows }, { data: movies }]) => {
       return {
-        // TODO: fix Text content does not match server-rendered HTML.
-        movie: movies?.results?.[random(0, movies.results.length)],
-        // TODO: Text content does not match server-rendered HTML.
-        tvShow: tvShows?.results?.[random(0, tvShows.results.length)],
+        movie: movies?.results?.[randomTrending ?? 0],
+        tvShow: tvShows?.results?.[randomTrending ?? 0],
       };
     },
     queries: [trendingTVOptions(), trendingMoviesOptions()],
   });
 
-  return movie && random(0, 1) === 0 ? (
-    <MovieHero movie={movie} />
-  ) : tvShow ? (
-    <TvShowHero tvShow={tvShow} />
-  ) : null;
-}
+  if (movie && shouldShowMovie) {
+    return <MovieHero movie={movie} />;
+  }
 
-export const Route = createFileRoute("/_layout/")({
-  component: Home,
-  loader: async ({ context }) => {
-    await context.queryClient.ensureQueryData(trendingMoviesOptions());
-    await context.queryClient.ensureQueryData(trendingTVOptions());
-  },
-});
+  return tvShow ? <TvShowHero tvShow={tvShow} /> : null;
+}
