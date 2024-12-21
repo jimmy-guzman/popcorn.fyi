7b0f952b280ba9abfb81dd9a95d594c869ace36b
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2024-12-21 16:32:24 -0600

    fix: 🐛 show friendler date for release dates (#78)
    🏁 Closes: #74

--- libs/utils/src/date.ts
+++ libs/utils/src/date.ts
@@ -3,3 +3,13 @@ const yearOnlyFormatter = new Intl.DateTimeFormat("en-US", { year: "numeric" });
 export const formatDateAsYearOnly = (date: string) => {
   return yearOnlyFormatter.format(new Date(date));
 };
+
+const longMonthFormatter = new Intl.DateTimeFormat("en-US", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
+export const formatDateAsLongMonth = (date: string) => {
+  return longMonthFormatter.format(new Date(date));
+};
