ad0ca8d7291f8943359621cab2027078d6c593bd
jimmy-guzman <hi@jimmyguzman.com>
2025-01-02 11:14:59 -0600

    fix: 🐛 show media type name in table

---
+++ apps/web/app/components/trending-carousel.tsx
@@ -0,0 +1,41 @@
+import { MovieHero } from "./movie-hero";
+import { PersonHero } from "./person-hero";
+import { TvShowHero } from "./tv-show-hero";
+
+export const TrendingCarousel = ({
+  trending,
+}: {
+  trending: { id: number; media_type?: string }[];
+}) => {
+  return (
+    <div className="dsy-carousel w-full">
+      {trending.map((result) => {
+        if (result.media_type === "tv") {
+          return (
+            <div className="dsy-carousel-item w-full" key={result.id}>
+              <TvShowHero tvShow={result} />
+            </div>
+          );
+        }
+
+        if (result.media_type === "movie") {
+          return (
+            <div className="dsy-carousel-item w-full" key={result.id}>
+              <MovieHero movie={result} />
+            </div>
+          );
+        }
+
+        if (result.media_type === "person") {
+          return (
+            <div className="dsy-carousel-item w-full" key={result.id}>
+              <PersonHero person={result} />
+            </div>
+          );
+        }
+
+        return null;
+      })}
+    </div>
+  );
+};
