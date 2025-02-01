739434dd4cb8947c486dc67820c73ced10d4a63c
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-02-01 09:28:32 -0600

    refactor: ♻️ move `<Input />` to UI lib (#201)

---
+++ libs/ui/src/input.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Input } from "./input";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  title: "Input",
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "Search...",
+    type: "text",
+  },
+};
