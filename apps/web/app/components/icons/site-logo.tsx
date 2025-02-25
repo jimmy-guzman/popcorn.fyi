50319edf4bb895bfc8da77db6ec68fb3b2cbed39
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-02-25 17:17:56 -0600

    refactor: ♻️ drop use of `xmlnsXlink` prop in svgs (#301)

--- apps/web/app/components/icons/site-logo.tsx
+++ apps/web/app/components/icons/site-logo.tsx
@@ -10,7 +10,6 @@ export const SiteLogo = ({ classname }: SiteLogoProps) => {
       version="1.0"
       viewBox="0 0 375 374.999991"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
       zoomAndPan="magnify"
     >
       <defs>
