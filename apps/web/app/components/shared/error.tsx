0ad943e7f74457237fffc8ae673e41166a53c541
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-03-08 09:28:23 -0600

    fix: 🐛 show sign in on protected route on initial load (#348)

--- apps/web/app/components/shared/error.tsx
+++ apps/web/app/components/shared/error.tsx
@@ -1,20 +1,11 @@
 import type { ErrorComponentProps } from "@tanstack/react-router";
 
-import { SignIn } from "@clerk/tanstack-start";
 import { Button } from "@popcorn.fyi/ui/button";
 import { Hero, HeroContent, HeroTitle } from "@popcorn.fyi/ui/hero";
 import { useRouter } from "@tanstack/react-router";
 
 export const Error = ({ error }: ErrorComponentProps) => {
   const router = useRouter();
-
-  if (error.message === "Not authenticated") {
-    return (
-      <div className="flex h-[calc(100vh-68px)] items-center justify-center">
-        <SignIn forceRedirectUrl={globalThis.location.href} routing="hash" />
-      </div>
-    );
-  }
 
   return (
     <main className="grid min-h-screen place-content-center">
