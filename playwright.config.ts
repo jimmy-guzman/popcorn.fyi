aa8ac07e2e7d1080b5b08849f8fcb87ab709a530
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-06-30 07:50:33 -0500

    refactor: ♻️ move to `@t3-oss/env-core` instead of custom (#514)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
 
 const IS_CI = process.env.CI;
-const THIRTY_MINUTES_MS = 1_800_000;
+const GLOBAL_TIMEOUT = 1_800_000;
 const CI_RETRIES = 3;
 
 export default defineConfig({
   forbidOnly: true,
   fullyParallel: true,
-  globalTimeout: IS_CI ? THIRTY_MINUTES_MS : undefined,
+  globalTimeout: IS_CI ? GLOBAL_TIMEOUT : undefined,
   projects: [
     {
       name: "chromium",
